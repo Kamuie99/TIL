4a673eb94fa359a67230bce3e4c8214317a9add0
이유찬 <dakgoo02@naver.com>
2024-01-18 16:15:35 +0900

    240118

---
+++ python_hw_4_4/hw_4_4.py
@@ -0,0 +1,44 @@
+list_of_book = [
+    '장화홍련전',
+    '가락국 신화',
+    '온달 설화',
+    '금오신화',
+    '이생규장전',
+    '만복자서포기',
+    '수성지',
+    '백호집',
+    '원생몽유록',
+    '홍길동전',
+    '장생전',
+    '도문대작',
+    '옥루몽',
+    '옥련몽',
+]
+
+rental_book = [
+    '장생전',
+    '위대한 개츠비',
+    '원생몽유록',
+    '이생규장전',
+    '데미안',
+    '장화홍련전',
+    '수성지',
+    '백호집',
+    '난중일기',
+    '홍길동전',
+    '만복자서포기',
+]
+
+missing_book = []
+
+count = 0
+for book in rental_book:
+    if book not in list_of_book:
+        missing_book.append(book)
+        count += 1
+
+if count == 0:
+    print("모든 도서 보유중")
+else:
+    for missing in missing_book:
+        print(f"{missing} 을/를 보충하여야 합니다.")
